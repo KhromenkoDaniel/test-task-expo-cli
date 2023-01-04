76bbd5edc164d453fc31df5eed5c9a37e84b9f86
KhromenkoDaniel <97105451+KhromenkoDaniel@users.noreply.github.com>
2023-01-05 00:28:47 +0200

    Add styles

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -12,8 +12,8 @@ const ImageCard = (props) => {
     return (
         <TouchableOpacity className="p-2" onPress={()=>handleClick()}>
             <Image source={{uri: itemData?.urls?.regular}} style={{width: 100, height: 100}}/>
-            <Text className="text-sm w-24 text-center">{itemData?.alt_description ? itemData?.alt_description.slice(0, -15) + '...' :'No description'}</Text>
-            <Text className="text-sm text-center">{itemData?.user?.name}</Text>
+            <Text className="text-sm font-medium w-24 text-center">{itemData?.alt_description ? itemData?.alt_description.slice(0, -15) + '...' :'No description'}</Text>
+            <Text className="text-sm font-semibold text-center">{itemData?.user?.name}</Text>
         </TouchableOpacity>
     );
 };
